4fadb01f2b78c75b0105065ebaa079cdbe261522
Aniket Pradhan <aniketpradhan6047@gmail.com>
2023-08-08 20:11:23 +0530

    Initial commit 3

--- db/conn.js
+++ db/conn.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv= require('dotenv');
+const path = require('path');
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
 const DB = process.env.DATABASE;
 
 mongoose.connect(DB).then(() => {
