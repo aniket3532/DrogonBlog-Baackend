7c141899536bb113132e855162de0fab92456919
Aniket Pradhan <aniketpradhan6047@gmail.com>
2023-08-08 19:12:08 +0530

    Initial commit 2

---
+++ db/conn.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB).then(() => {
+    console.log('connection successful');
+}).catch((err) => {
+    console.log(`connection error: ${err.message}`);
+}) ;
+
+//const { MongoClient } = require('mongodb')
+
+// let dbConnection
+
+// module.exports = {
+//   connectToDb: (cb) => {
+//     MongoClient.connect(DB)
+//       .then(client => {
+//         dbConnection = client.db()
+//         console.log('Connection established');
+//         return cb()
+//       })
+//       .catch(err => {
+//         console.log(err)
+//         return cb(err)
+//       })
+//   },
+//   getDb: () => dbConnection
+// }
+    
